b85e6448b441f7c392c5e8708d9ad6fb68e1c5d9
kyeahen <bright19961@naver.com>
2019-09-06 12:28:15 +0900

    프로젝트 세팅

---
+++ Vcheck/Vcheck/Home/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Vcheck
+//
+//  Created by 김예은 on 05/09/2019.
+//  Copyright © 2019 kyeahen. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
