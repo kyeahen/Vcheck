7d34aabfade885f4ea0c344ffe5c261d7829b807
장용범 <dnwkqk@gmail.com>
2019-09-26 12:35:22 +0900

    이미지 추가 / Pay - 화면전환, 이미지화면 완료

---
+++ Vcheck/Vcheck/Pay/PayCardCollectionViewCell.swift
@@ -0,0 +1,14 @@
+//
+//  PayCardCollectionViewCell.swift
+//  Vcheck
+//
+//  Created by 장용범 on 26/09/2019.
+//  Copyright © 2019 kyeahen. All rights reserved.
+//
+
+import UIKit
+
+class PayCardCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var cardimage: UIImageView!
+}
---
+++ Vcheck/Vcheck/Pay/PayCardViewController.swift
@@ -0,0 +1,43 @@
+//
+//  PayCardViewController.swift
+//  Vcheck
+//
+//  Created by 장용범 on 26/09/2019.
+//  Copyright © 2019 kyeahen. All rights reserved.
+//
+
+import UIKit
+
+class PayCardViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        setNavigationBar()
+
+        // Do any additional setup after loading the view.
+    }
+    @IBAction func ok(_ sender: Any) {
+        if #available(iOS 13.0, *) {
+            let navi = storyboard?.instantiateViewController(identifier: "payCompleteNavi") as! UINavigationController
+            present(navi, animated: true, completion: nil)
+        } else {
+            // Fallback on earlier versions
+        }
+    }
+    
+    @IBAction func close(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+    
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
