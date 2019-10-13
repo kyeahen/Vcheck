31d7e2ba60ef9e5fbf42a0bb3cbb60e76db4fa21
kyeahen <bright19961@naver.com>
2019-10-13 15:44:05 +0900

    이미지 검색 - gif 완료 / 유형 - 뷰 수정

--- Vcheck/Vcheck/Home/ViewController.swift
+++ Vcheck/Vcheck/Home/ViewController.swift
@@ -186,45 +186,38 @@ class ViewController: UIViewController {
     
     //MARK: 이미지 검색 액션
     @IBAction func imageSearchAction(_ sender: UIButton) {
+
         
-        
-        /* UIAlertController는 alert(우리가 흔히 보는 팝업창), actionSheet을 지원합니다.
-        우리는 actionSheet을 사용할 것이기 때문에 actionSheet Style의 UIAlertController 객체를 생성합니다. */
-        
-        let actionSheet = UIAlertController(title: "", message: "방법을 선택해주세요.", preferredStyle: .actionSheet)
-        
-        /* ActionSheet에서 사용할 Action을 정의합니다.
-        Action Style는 default, cancle, destructive를 지원합니다. */
+        let actionSheet = UIAlertController(title: nil, message: nil, preferredStyle: .actionSheet)
+        actionSheet.view.tintColor = #colorLiteral(red: 0, green: 0.7113869786, blue: 0.5025657415, alpha: 1)
+
         let cameraAction = UIAlertAction(title: "사진 촬영", style: .default, handler : {
             (action) in
             
-            //handler에는 해당 Action을 누르면 동작해야할 코드를 작성합니다.
             self.openCamera()
         })
         let galleryAction = UIAlertAction(title: "앨범에서 사진 선택", style: .default, handler : {
             (action) in
             
-            //handler에는 해당 Action을 누르면 동작해야할 코드를 작성합니다.
             self.openGallery()
         })
-        //취소 액션에는 딱히 동작할 코드가 없기 때문에 그냥 nil로 설정합니다.
+
         let cancelAction = UIAlertAction(title: "취소", style: .cancel, handler: nil)
         
-        //ActionSheet 객체에 위에서 정의한 3개의 액션을 추가합니다.
         actionSheet.addAction(cameraAction)
         actionSheet.addAction(galleryAction)
         actionSheet.addAction(cancelAction)
         
-        //화면에 ActionSheet을 띄웁니다.
         self.present(actionSheet, animated: true, completion: nil)
     }
     
     func imageSearch(selectedImage: UIImage) {
         ImageUploadService.postImage(image: selectedImage) { (value) in
-            if value.food_name == "머드쉐이크 초콜릿" {
+            if value.food_name == "머드쉐이크 초콜릿" { //X
                 let vc = UIStoryboard(name: "ImageSearch", bundle: nil).instantiateViewController(withIdentifier: "ImageSearchXViewController") as! ImageSearchXViewController
                 
                 vc.products = value
+                vc.productImage = selectedImage
                 vc.modalPresentationStyle = .fullScreen
                 self.present(vc, animated: true, completion: nil)
                 print(value)
@@ -232,7 +225,8 @@ class ViewController: UIViewController {
             } else if value.food_name == "블루문 벨지움 화이트" {
                 let vc = UIStoryboard(name: "ImageSearch", bundle: nil).instantiateViewController(withIdentifier: "ImageSearchViewController") as! ImageSearchViewController
                 
-                vc.products  = value
+                vc.products = value
+                vc.productImage = selectedImage
                 vc.modalPresentationStyle = .fullScreen
                 self.present(vc, animated: true, completion: nil)
                 print(value)
@@ -240,21 +234,24 @@ class ViewController: UIViewController {
             } else if value.food_name == "수미칩 오리지널" {
                 let vc = UIStoryboard(name: "ImageSearch", bundle: nil).instantiateViewController(withIdentifier: "ImageSearchViewController") as! ImageSearchViewController
                 
-                vc.products  = value
+                vc.products = value
+                vc.productImage = selectedImage
                 vc.modalPresentationStyle = .fullScreen
                 self.present(vc, animated: true, completion: nil)
                 print(value)
             } else if value.food_name == "버드와이저" {
                 let vc = UIStoryboard(name: "ImageSearch", bundle: nil).instantiateViewController(withIdentifier: "ImageSearchViewController") as! ImageSearchViewController
                 
-                vc.products  = value
+                vc.products = value
+                vc.productImage = selectedImage
                 vc.modalPresentationStyle = .fullScreen
                 self.present(vc, animated: true, completion: nil)
                 print(value)
-            } else if value.food_name == "포카칩 어니언" {
+            } else if value.food_name == "포카칩 어니언" { //X
                 let vc = UIStoryboard(name: "ImageSearch", bundle: nil).instantiateViewController(withIdentifier: "ImageSearchXViewController") as! ImageSearchXViewController
                 
-                vc.products  = value
+                vc.products = value
+                vc.productImage = selectedImage
                 vc.modalPresentationStyle = .fullScreen
                 self.present(vc, animated: true, completion: nil)
                 print(value)
@@ -262,6 +259,7 @@ class ViewController: UIViewController {
                 let vc = UIStoryboard(name: "ImageSearch", bundle: nil).instantiateViewController(withIdentifier: "ImageSearchXViewController") as! ImageSearchXViewController
                 
                 vc.products = value
+                vc.productImage = selectedImage
                 vc.modalPresentationStyle = .fullScreen
                 self.present(vc, animated: true, completion: nil)
                 print(value)
@@ -375,14 +373,11 @@ extension ViewController: UIImagePickerControllerDelegate, UINavigationControlle
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
         if let editedImage = info[.editedImage] as? UIImage {
-//            imageView.image = editedImage
-//            imageView.image = editedImage
+
             self.imageSearch(selectedImage: editedImage)
             self.imagePickerFlag = true
 
         } else if let selectedImage = info[.originalImage] as? UIImage {
-//            imageView.image = selectedImage
-//            imageView.image = selectedImage
             
             self.imageSearch(selectedImage: selectedImage)
             self.imagePickerFlag = true
